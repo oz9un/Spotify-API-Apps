41ebfa03a3de9dc32e5e64704cb843e6b413a6c6
oz9un <okultekin@etu.edu.tr>
2021-12-01 23:14:07 +0300

    Argparse implemented, now user can use --help option

--- refreshOauthToken.py
+++ refreshOauthToken.py
@@ -1,4 +1,4 @@
-import requests, json, base64, sys
+import requests, json, base64, sys, argparse
 
 class SpotifyAPI:
     def __init__(self, refresh_token, client_id, client_secret):
@@ -22,10 +22,17 @@ class SpotifyAPI:
 
 
 if __name__ == '__main__':
-    client_id = sys.argv[1]
-    client_secret = sys.argv[2]
-    refresh_token = sys.argv[3]
-    spotify_api = SpotifyAPI(refresh_token, client_id, client_secret)
+    # Get user parameters:
+    parser = argparse.ArgumentParser()
+
+    # Define parameters
+    parser.add_argument("-id", "--client_id", required=True, help="Client ID of Spotify account, obtained from Spotify developer dashboard.")
+    parser.add_argument("-secret", "--client_secret", required=True, help="Client Secret of Spotify account, obtained from Spotify developer dashboard.")
+    parser.add_argument("-token", "--refresh_token", required=True, help="Refresh token, obtained after authorization process.")
+
+    args = vars(parser.parse_args())
+
+    spotify_api = SpotifyAPI(args["refresh_token"], args["client_id"], args["client_secret"])
 
     access_token = spotify_api.RequestAccessToken()
     print("Your new access token is: " + access_token)
